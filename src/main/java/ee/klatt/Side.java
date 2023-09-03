7985c0ae13d8e694b6a8f882e175c59168e431ed
Mateusz Klatt <mateusz@klatt.ie>
2023-09-03 15:37:02 +0200

    Add enum values

--- src/main/java/ee/klatt/Side.java
+++ src/main/java/ee/klatt/Side.java
@@ -1,6 +1,16 @@
 package ee.klatt;
 
 public enum Side {
-    Buy,
-    Sell
+    Buy(1),
+    Sell(-1);
+    private final int value;
+
+    Side(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
 }
